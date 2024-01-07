8e97b8f6647367690b1420e762672bae7af1e89f
rachmaliaputris <rachmalia.putri@mhs.itenas.ac.id>
2024-01-07 22:22:42 +0700

    fixing

--- resources/views/artikel.blade.php
+++ resources/views/artikel.blade.php
@@ -41,7 +41,6 @@
                                         </a>
                                         
                                     @endif
-                                
                                 </p>
                             </div>
                         </div>
