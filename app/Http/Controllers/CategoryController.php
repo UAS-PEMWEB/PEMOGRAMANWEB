43cb4cc2c8420bd099ccbdc41898030eb6c81b84
nuri <nuraiini604@gmail.com>
2024-01-08 08:04:01 +0700

    fix

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -23,7 +23,6 @@ class CategoryController extends Controller
     function create() {
         return view('admin.pages.categories.create');
     }
-
     public function store(Request $request)
     {
         $validatedData = $request->validate([
