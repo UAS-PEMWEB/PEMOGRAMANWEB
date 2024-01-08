7190f3a10992c3264f9bb680165ed8cd7baaa977
rachmaliaputris <rachmalia.putri@mhs.itenas.ac.id>
2024-01-08 09:00:21 +0700

    fix

--- resources/views/artikel.blade.php
+++ resources/views/artikel.blade.php
@@ -52,7 +52,6 @@ Artikel
                         {{ $artikels->links() }}
                     </div> 
                 </div>
-
                 <!-- Blog Sidebar -->
                 <div class="col-lg-4 col-md-12 blog-sidebar theiaStickySidebar">
                 <!-- Search -->
